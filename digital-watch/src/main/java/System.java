5dfdb9145923ee6acd752f972adf41dae0f0b4c7
APinCan <6794ch@naver.com>
2019-05-20 00:38:06 +0900

    시퀀스다이어그램 delete alarm하는중

--- digital-watch/src/main/java/System.java
+++ digital-watch/src/main/java/System.java
@@ -39,6 +39,11 @@ public class System extends JFrame {
 //    private String[] selectedModes; //원래 String[]인데 arraylist로 수정
     private ArrayList<String> selectedModes = new ArrayList<>();     //modeselector로 넘겨줄선택된 모드들을 나타냄, 그러니까 현재 선택된 모드들
     private TimeDB timeDB; //System이 TimeDB에서 값을 받아오는게 존재
+    private boolean timekeepingAdjustState=false; //timekeeping에서 adjust버튼을 누를경우 state가 true로 바뀌어 시간을 조정중임을 알림
+    private boolean timerAdjustState=false; //timer에서 adjust버튼을 누를경우 state가 true로 바뀌면서 timer조정가능
+    private int timerRunState=0; //시퀀스다이어그램상에서 int이길래 일단 int로 설정 근데 boolean이 더 맞는것같음
+    private boolean alaramAdjustState=false; //alarm에서 adjust버튼을 누를경우 state가 true로 바뀌면서 alarm조정가능
+    private boolean alarmCanAddState=false; //alarm에 alarm을 더 추가시킬 수 있을경우
 
 
     public System() {
@@ -54,21 +59,141 @@ public class System extends JFrame {
     //액션리스너의 경우 버튼에 직접 다는게 가능하긴함
     private void btnAdjustActionPerformed(ActionEvent e) {
         // TODO add your code here
-        //currnetMode가 timekeeping일경우 adjust버튼을 누른다면
-        reqAdjustTime(); //으로 진행
-
+        if(currentMode.equals("TimeKeeping")){
+            //adjust 페이즈가 아닐경우 adjusttime으로 진행
+            if(!timekeepingAdjustState) {
+                //currnetMode가 timekeeping일경우 adjust버튼을 누른다면
+                reqAdjustTime(); //으로 진행
+            }
+            //adjust페이즈 일경우 adjustbutton을 누르면 adjust페이즈를 종료
+            else{
+                endAdjustTime();
+            }
+        }
+        else if(currentMode.equals("Timer")){
+            //adjust페이즈가 아닐경우 adjust페이즈로 진입
+            if(!timerAdjustState){
+                reqSetTimer();
+            }
+            //adjust페이즈에서 빠져나올때
+            else{
+                endSetTimer();
+            }
+        }
+        else if(currentMode.equals("Alarm")){
+            //alarm모드에서 알람을 조정중이지 않은 경우
+            if(!alaramAdjustState) {
+                //adjsut버튼을 누를경우 addAlarm실행
+                reqAddAlarm();
+            }
+            //alamr모드에서 알람을 조정중인데 빠져나오려고 할 경우
+            else{
+                endAddAlarm();
+            }
+        }
     }
 
     private void btnResetActionPerformed(ActionEvent e) {
         // TODO add your code here
+        if(currentMode.equals("TimeKeeping")){
+            //뭔가 들어갈게 있겠지
+        }
+        else if(currentMode.equals("Timer")){
+            //Timer모드이긴한데 현재 타이머가 돌아가는중일경우 reset버튼을 누르면 일단 타이머를 멈춤
+            if(timerRunState==1){
+                //timer.pauseTimer(); pauseTimer에서 현재 system의 timerRunState=0;으로 만들어주는게 필요
+            }
+            //timer모드이고 현재타이머의 스테이트가 1 이 아니면(0이면: a.k.a timer가 정지해있는경우) reset가능
+            else{
+                reqResetTimer();
+            }
+        }
+        else if(currentMode.equals("Alarm")){
+            //alarm모드인데 현재 알람 조정상태가 아닌경우 reset버튼을 누르면 설정된 알람을 제거
+            if(!alaramAdjustState){
+                reqDeleteAlarm();
+            }
+            /*
+            현재 바로 아래의 else부분의 usecase가 정의되어 있지 않음
+             */
+            //alarm모드인데 현재 알람 조정상태일경우 reset버튼을 누르면 현재 설정하려고하는 알람을 00:00:00으로 초기화???
+            else{
+                //(대충 알람을 시간을 0으로 초기화한다는 내용)
+            }
+        }
     }
 
     private void btnModeActionPerformed(ActionEvent e) {
         // TODO add your code here
+        if(currentMode.equals("TimeKeeping")){
+            //TimeKeeping모드이고 adjust time일때 mode버튼을 누르면 cursor가 바뀜
+            if(timekeepingAdjustState) {
+                //changeCurosr 실행시 커서의 위치를 변경시켜주는것도 필요
+                changeCursor();
+            }
+            //timekeeping모드이고 adjusttime이 아닐때 mode버튼 누르면 다음 모드로 감
+            else{
+                //(대충 다음 모드로 간다는 코드)
+            }
+        }
+        else if(currentMode.equals("Timer")){
+            //Timer모드이고 timer를 adjust중일때 mode버튼을 누르면 cursor가 바뀜
+            if(timerAdjustState) {
+                //changeCurosr 실행시 커서의 위치를 변경시켜주는것도 필요
+                changeCursor();
+            }
+            //timer모드이고 settimer모드가 아닐때 mode버튼을 누르면 다음모드로 감
+            else{
+                //(대충 다음 모드로 간다는 코드)
+            }
+        }
+        else if(currentMode.equals("Alarm")){
+            //alarm모드이고 alarm이 설정가능상태일경우 mode버튼을 누르면 cursor가 바뀜
+            if(alaramAdjustState){
+                changeCursor();
+            }
+            //alarm모드이고 alarm설정상태가 아닐때 mode버튼을 누르면 다음 모드로
+            else {
+                //(대충 다음 모드로 간다는 코드)
+            }
+        }
     }
 
     private void btnStartActionPerformed(ActionEvent e) {
         // TODO add your code here
+        if(currentMode.equals("TimeKeeping") && timekeepingAdjustState){
+            //현재 커서의 위치가 어디인지를 알수있는 방법 필요
+            increaseTime(); // TimeKeeping모드이고 adjust모드일때 현재 커서위치의 값을 증가시킴
+        }
+        else if(currentMode.equals("Timer")){
+            //Timer모드이고 adjust모드일때 start버튼을 누르면 현재 커서 위치의 값 증가
+            if(timerAdjustState && timerRunState==0) {
+                //현재 커서의 위치가 어디인지를 알수있는 방법 필요
+                increaseTime();
+            }
+            /* 중요?
+            전제 조건 = Timer상태이고 adjust모드가 아니여야 timer의 start가 가능하다
+             */
+            //Timer모드이고 adjust모드가 아닐때 start버튼을 누르면 타이머가 동작
+            else if(!timerAdjustState && timerRunState==0){ //tiemerAdjustState가 항상 true라는 경고문, 일단 참고
+                reqStartTimer();
+            }
+            //Timer모드이고 adjust모드가 아니고 Timer가 동작중일때 start버튼을 누르면 타이머가 멈춤
+            else if(!timerAdjustState && timerRunState==1){
+                reqPauseTimer();
+            }
+        }
+        else if(currentMode.equals("Alarm")){
+            //alarm모드이고 alarm 설정상태가 아닐경우 start버튼을 누르면 다음알람을 출력
+            if(!alaramAdjustState){
+                reqNextAlarm();
+            }
+            //alarm상태이고 alarm설정상태일경우 start버튼을 누르면 설정하는 알람의 현재 커서의 위치값 증가
+            else if(alaramAdjustState){
+                increaseTime();
+            }
+
+        }
     }
 
     private void initComponents() {
@@ -179,42 +304,62 @@ public class System extends JFrame {
 
     }
 
+    //현재 커서 위치에 있는 값을 증가시킴
+    //사용하는 모드들 = TimeKeeping, Timer
+    public void increaseTime(){
+        //현재커서위치의 값
+        //currentNumber++;
+    }
+
     public void showTimer(){
 
     }
 
     public void reqSetTimer(){
+        //기능들 필요
 
+        this.timerAdjustState=true;
     }
 
     public void increaseTimer(){
 
     }
 
-    public void changeCursor(){
+    private void changeCursor(){
 
     }
 
-    public void endSetTimer(){
-
+    private void endSetTimer(){
+        this.timerAdjustState=false;
     }
 
-    public void reqAdjustTime(){
+    private void reqAdjustTime(){
         //timekeeping모드에서 adjust버튼을 누르면 이쪽으로
 
         //timeDB.getTime()메서드 실행
-    }
 
-    public void endAdjustTime(){
-
-    }
-
-    public void reqStartTimer(){
+        //Adjsuttime으로 들어왔다면 timekeepingAdajsutState를 true로 바꾸고
+        if(!timekeepingAdjustState){
+            this.timekeepingAdjustState=true;
+        }
 
     }
 
-    public void reqResetTimer(){
+    //현재 adjust페이즈일경우 그걸 종료시키는것
+    private void endAdjustTime(){
+        if(timekeepingAdjustState){
+            this.timekeepingAdjustState=false;
+        }
+    }
 
+    private void reqStartTimer(){
+        //Timer를 가동시켜주는 코드가 필요
+    }
+
+    //reset을 시켜주면 타이머의 시간을 0으로 초기화
+    //timer의 시간이 00:00:00이여도 상관없이 0으로 << 이게 편함
+    private void reqResetTimer(){
+        //timer의 시간을 0으로
     }
 
     public void showAlarm(){
@@ -225,20 +370,32 @@ public class System extends JFrame {
 
     }
 
-    public void reqNextAlarm(){
-
+    private void reqNextAlarm(){
+        //나중에 구현해야함
     }
 
-    public void reqAddAlarm(){
-
+    //alarm화면에서  adjust버튼을 누르면 실행
+    private void reqAddAlarm(){
+        //만약 알람리스트에 알람이 가득 차있을경우
+        if(alarmList.length>=4){
+            //알람을 더이상 추가할수없다는 메세지 출력하기
+            alarmCanAddState=false;
+            alaramAdjustState=false;
+        }
+        //만약 알람리스트에 있는 알람이 4개가 아닐경우(알람을 더 넣을수 있을경우)
+        else{
+            alarmCanAddState=true;
+            alaramAdjustState=true;
+        }
     }
 
-    public void endAddAlarm(){
-
+    private void endAddAlarm(){
+        this.alaramAdjustState=false;
+        alarmCanAddState=false;
     }
 
-    public void reqDeleteAlarm(){
-
+    private void reqDeleteAlarm(){
+        //(대충 현재 설정된 알람을 제거한다는 코드)
     }
 
     public void reqStopAlarm(){
@@ -315,4 +472,9 @@ public class System extends JFrame {
         }
     }
 
+    //시퀀스에는 있는데 클래스다이어그램에는 없는건가?
+    //동작중인 Tiemer를 멈추는 역할
+    private void reqPauseTimer(){
+
+    }
 }
