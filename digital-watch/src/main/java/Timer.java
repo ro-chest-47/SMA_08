9ea5d62e08ba8f3e38a402c64c8f587ca4c0e8ec
dkdh <48465471+dkdh@users.noreply.github.com>
2019-05-24 19:36:55 +0900

    성능개선

--- digital-watch/src/main/java/Timer.java
+++ digital-watch/src/main/java/Timer.java
@@ -1,87 +1,114 @@
 import java.awt.Toolkit;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Timer extends Thread {
 	private static int hours;
 	private static int minutes;
 	private static int seconds;
-	
+
 	private static int runState =0;
 	private static int zeroState=0;
-	
+
 	private Thread thread;
-	
+	private Timer runnable;
+	ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+
 	private int time =0;
-	
+
+	private class Buzzer extends Thread{
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		private int i =0;
+		public void run() {
+			while(true) {
+				toolkit.beep();
+				i++;
+				try {
+					Thread.sleep(1000);
+				}catch (InterruptedException e) {break;}
+				if(i == 3)
+					break;
+			}
+
+		}
+
+	}
+
 	private Timer() {
 		setTimer("0 0 0");
 	}
-	
+
 	private Timer(String time) {
 		setTimer(time);
 	}
-	
+
 	public static Timer getInstance() {
 		return LazyHolder.INSTANCE;
 	}
-	
+
 	private static class LazyHolder{
 		private static final Timer INSTANCE = new Timer();
 	}
-	
+/*
 	public void startUpdateTime(Thread timeThread) {
 		timeThread.start();
-	}
-	
+	}*/
+
 	public int getRunState() {
 		return this.runState;
 	}
-	
+
 	public int getZeroState() {
 		return this.zeroState;
 	}
-	
+
 	public String getTime() {
 		String time= Integer.toString(hours)+" "+Integer.toString(minutes)+" "+Integer.toString(seconds);
-		
+
 		return time;
 	}
-	
+
 	public void pauseTimer() {
-		thread.interrupt();
+		service.shutdown();
+		//System.out.println(this.getTime());
 		this.runState=0;
 		if(this.getTime().equals("0 0 0"))
 			zeroState=1;
 	}
-	
+
 	public void setTimer(String time) {
 		String[] times = time.split("\\s");
 
 		this.hours=Integer.parseInt(times[0]);
 		this.minutes=Integer.parseInt(times[1]);
 		this.seconds=Integer.parseInt(times[2]);
-		
+
 		if(time.equals("0 0 0")) {
 			zeroState=1;
 		}
 		else zeroState=0;
 	}
-	
+
 	public void startTimer() {
-		thread = new Timer(this.getTime());
+		runnable = new Timer(getTime());
+		thread = new Thread(runnable);
+		service.scheduleAtFixedRate(runnable, 0, 10, TimeUnit.MILLISECONDS);
 		this.runState=1;
-		startUpdateTime(thread);
+		//startUpdateTime(thread);
 	}
-	
+
 	public void resetTimer() {
 		setTimer("0 0 0");
 		zeroState=1;
 	}
-	
+
 	public void buzzTimer() {
-		Toolkit toolkit=Toolkit.getDefaultToolkit();
-		toolkit.beep();
+		Buzzer buzzer = new Buzzer();
+
+		buzzer.start();
 	}
-	
+
 	public void updateTime() {
 		this.time++;
 		if(this.time==10) {
@@ -101,24 +128,14 @@ public class Timer extends Thread {
 			setTimer("0 0 0");
 			buzzTimer();
 		}
-		
+
 	}
-	
-	public void run() {
-		while(true) {
-			try {
-				if(this.time==0)
-					System.out.println(this.getTime());
-				
-				updateTime();
-				
-				if(this.zeroState==1)
-					break;
-					
-				Thread.sleep(100);
-			}catch (InterruptedException e) {break;}
-		}
-		
+
+	public void run(){
+		updateTime();
+		System.out.println(this.getTime());
+
+
 	}
-	
+
 }
