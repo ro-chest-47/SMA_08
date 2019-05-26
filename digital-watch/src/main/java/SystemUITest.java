cc9371650896a437305dd5fb204060da7c008e61
APinCan <6794ch@naver.com>
2019-05-26 20:22:11 +0900

    systemUI테스트 클래스 작성

---
+++ digital-watch/src/main/java/SystemUITest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import javax.swing.*;
+
+import static org.junit.Assert.*;
+
+public class SystemUITest {
+
+    @Test
+    public void main() {
+        SystemUI systemUI = new SystemUI();
+
+        assertNotNull(systemUI);
+    }
+
+    @Test
+    public void run() {
+    }
+}
