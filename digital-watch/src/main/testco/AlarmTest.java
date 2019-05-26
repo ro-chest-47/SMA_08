882a4c657aa3e29aacba35bdd7f14811fc5ced24
APinCan <6794ch@naver.com>
2019-05-26 22:17:17 +0900

    테스트폴더 추가

---
+++ digital-watch/src/main/testco/AlarmTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AlarmTest {
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void deleteInstance() {
+    }
+
+    @Test
+    public void addAlarm() {
+    }
+
+    @Test
+    public void nextAlarm() {
+    }
+
+    @Test
+    public void deleteAlarm() {
+    }
+
+    @Test
+    public void buzzAlarm() {
+    }
+
+    @Test
+    public void buzzBuzzer() {
+    }
+
+    @Test
+    public void startAlarm() {
+    }
+
+    @Test
+    public void stopAlarm() {
+    }
+
+    @Test
+    public void getAlarmList() {
+    }
+}
