05a424563bc423062f003213d491ea38f74e42be
Jeong <jijon1214@gmail.com>
2019-06-08 11:26:52 +0900

    넹면

--- digital-watch/src/main/java/SystemUI.java
+++ digital-watch/src/main/java/SystemUI.java
@@ -1,5 +1,8 @@
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
